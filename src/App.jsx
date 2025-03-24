3b2b1eb8e5c7feb651f2f9e946257f24333eed06
Vicente Cantú <vice.00@hotmail.com>
2025-03-24 13:14:10 -0600

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
