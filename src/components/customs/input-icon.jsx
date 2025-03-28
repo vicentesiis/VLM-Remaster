a14b328c151bf10fe684d08e69dd83a5a3f77491
Vicente Cantú <vice.00@hotmail.com>
2025-03-28 07:37:30 -0600

    Date Range Picker n InputIcon added.

---
+++ src/components/customs/input-icon.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+
+export function InputIcon({ className, placeholder, icon: Icon, ...props }) {
+  return (
+    <div className="relative">
+      <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground">
+        {Icon && <Icon className="mr-2.5 h-4 w-4" />}
+      </div>
+      <Input
+        id="search"
+        type="search"
+        placeholder={placeholder}
+        className="w-full rounded-lg bg-background pl-8"
+      />
+    </div>
+  )
+}
+
+export default InputIcon
--- src/pages/main/tasks/tasks.jsx
+++ src/pages/main/tasks/tasks.jsx
@@ -5,31 +5,20 @@ import PageLayout from "@/components/customs/page-layout"
 import { Filter } from "@/components/customs/filter"
 import { FilterTableLayout } from "@/components/customs/table-data/filter-table-layout"
 import { SearchIcon } from "lucide-react"
-import { Input } from "@/components/ui/input"
 import { FancyMultiSelect } from "@/components/customs/fancy-multi-select"
-import { InputWithIcon } from "@/components/ui/input"
+import { DateRangePicker } from "@/components/customs/date-range-picker/date-range-picker"
+import InputIcon from "@/components/customs/input-icon"
 
 export const Tasks = () => {
-  //   <div className="mb-4 flex flex-col space-y-2 sm:flex-row sm:items-center sm:justify-between sm:space-x-16 sm:space-y-0">
-  //   <div className="flex flex-col space-y-2 sm:grow sm:flex-row sm:items-center sm:space-x-4">
-  //     <div className="flex w-full max-w-sm items-center rounded-md border border-input px-2.5 py-1.5">
-  //       <SearchIcon className="mr-2.5 h-4 w-4" />
-  //       <Input
-  //         type="search"
-  //         placeholder="Buscar tareas"
-  //         className="border-0 shadow-none lg:w-96"
-  //       />
-  //     </div>
-  //     <FancyMultiSelect className="lg:w-96" />
-  //   </div>
-  //   <div className="flex justify-end">
-  //     <Button>Buscar</Button>
-  //   </div>
-  // </div>
   function TaskFilter() {
     return (
       <Filter>
-        <InputWithIcon icon={SearchIcon} placeholder="Buscar Tareas"  />
+        <InputIcon placeholder={"Buscar"} icon={SearchIcon} />
+        <DateRangePicker
+          onUpdate={(values) => console.log(values)}
+          locale="en-GB"
+          showCompare={false}
+        />
         <FancyMultiSelect />
       </Filter>
     )
