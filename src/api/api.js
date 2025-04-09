eeaaf1635dc0946547a8105b7603afaec68336ca
Vicente Cantú <vice.00@hotmail.com>
2025-04-09 17:49:58 -0600

    API connected to the server.

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api"
+const API_URL = "/api"
 
 const apiRequest = async (endpoint, method, headers, body) => {
   if (body && headers["Content-Type"] === "application/json") {
