03fcccab0a46245590b57d4e627614edab687956
Vicente Cantú <vice.00@hotmail.com>
2025-06-17 19:33:09 -0600

    second try for render.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import react from "@vitejs/plugin-react"
+import path from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 })
