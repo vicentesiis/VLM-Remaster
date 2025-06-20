beb4b55154b6ffc8bb8302fb8740f0bc1b949c3f
Vicente Cantú <vice.00@hotmail.com>
2025-06-20 15:13:15 -0600

    validations when is isAgent in Usuarios.

--- src/pages/main/usuarios/components/group-responsible.jsx
+++ src/pages/main/usuarios/components/group-responsible.jsx
@@ -1,16 +1,23 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { useCurrentUser } from "@/hooks/useCurrentUser"
 import UserRoleCard from "@/pages/main/usuarios/components/user-role-card"
 
-export const GroupResponsible = ({ admin, leader }) => (
-  <div className="flex w-[270px] flex-col gap-2">
-    <p className="text-lg font-normal">Responsables del Equipo</p>
-    <div className="flex flex-col gap-4">
-      <UserRoleCard {...admin} role="admin" />
-      <UserRoleCard {...leader} role="lider" />
+export const GroupResponsible = ({ admin, leader }) => {
+  const { isAgent } = useCurrentUser()
+
+  return (
+    <div className="flex w-[270px] flex-col gap-2">
+      <p className="text-lg font-normal">
+        {isAgent ? "Líder del Equipo" : "Responsables del Equipo"}
+      </p>
+      <div className="flex flex-col gap-4">
+        {!isAgent && <UserRoleCard {...admin} role="admin" />}
+        <UserRoleCard {...leader} role="lider" />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 GroupResponsible.propTypes = {
   admin: PropTypes.any,
