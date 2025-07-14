94e3cb2c9ca3962bdb0136c2a348babdb46109b9
Vicente Cantú <vice.00@hotmail.com>
2025-07-14 10:04:56 -0600

    env variables.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,7 +5,7 @@ import {
   logout,
 } from "@/services/authService"
 
-const API_URL = "https://vl-api-v6.onrender.com/api"
+const API_URL = import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
