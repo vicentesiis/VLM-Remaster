13f740e29d893150b38c07dfff7e8c783aabb1a5
Vicente Cantú <vice.00@hotmail.com>
2025-07-14 10:40:14 -0600

    redirect to login when user is 404 or 401

--- src/api/plainAxios.js
+++ src/api/plainAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const plainAxios = axios.create({
-  baseURL: "https://vl-api-v6.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: "application/json",
   },
--- src/context/auth-context.jsx
+++ src/context/auth-context.jsx
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import PropTypes from "prop-types"
 import React, { createContext, useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import FullScreenLoader from "@/components/customs/full-screen-loader"
 import { useCurrentUser } from "@/hooks/queries/useUser"
 import { loginUser, logout as logoutUser } from "@/services/authService"
@@ -10,12 +11,27 @@ export const AuthContext = createContext(null)
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("access_token"))
   const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  console.log("AuthProvider token:", token)
 
   const {
     data: user,
     isError,
     isLoading: isUserLoading,
+    error,
   } = useCurrentUser({ enabled: !!token })
+
+  useEffect(() => {
+    if (
+      isError &&
+      (error?.response?.status === 401 || error?.response?.status === 404)
+    ) {
+      localStorage.removeItem("access_token")
+      setToken(null)
+      navigate("/login")
+    }
+  }, [isError, error, navigate])
 
   const loading = isUserLoading
 
